6903aced03e8faf7cefa1dffe8b8a18d8c291ddf
garza-utsa <john.garza@utsa.edu>
2023-10-31 15:33:16 -0500

    fixing default error on import

--- src/js/bundle/college-dls-umd.js
+++ src/js/bundle/college-dls-umd.js
@@ -2,7 +2,7 @@ import * as jQuery from '../../../node_modules/jquery/dist/jquery';
 import * as select2 from '../../../node_modules/select2/dist/js/select2';
 import * as magnific from '../../../node_modules/magnific-popup/dist/jquery.magnific-popup';
 import * as Popper from '../../../node_modules/@popperjs/core/dist/umd/popper';
-import BlazeSlider from "../../../node_modules/@glidejs/glide/dist/glide";
+import * as BlazeSlider from "../../../node_modules/@glidejs/glide/dist/glide";
 
 // from 'js/src/*' source which works
 // import Alert from '../../node_modules/bootstrap/js/src/alert';
@@ -31,7 +31,8 @@ export default {
     Tab,
     Toast,
     Popper,
-    Tooltip
+    Tooltip,
+    Glide
 }
 
 // Navbar Toggle
