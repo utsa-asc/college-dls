a8157e6240dbcda393f5522c4c2492e886e4642d
Lallo Vigil <lallo.vigil@gmail.com>
2023-11-03 17:30:35 -0500

    Added back to top functionality

--- src/js/bundle/college-dls-umd.js
+++ src/js/bundle/college-dls-umd.js
@@ -33,6 +33,27 @@ export default {
     Tooltip
 }
 
+    // BEGIN: BACK TO TOP LINK
+    $("#top-link").hide();
+    // COMMENT: ADDS SCROLL FUNCTIONALITY TO #top-link ELEMENTS BACK TO TOP BUTTON/LINK
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $("#top-link").fadeIn();
+      } else {
+        $("#top-link").fadeOut();
+      }
+    });
+    $("#top-link").click(function () {
+      $("html, body").animate(
+        {
+          scrollTop: 0,
+        },
+        600
+      );
+      return false;
+    });
+    // END: BACK TO TOP LINK
+
 // Navbar Toggle
 $(document).ready(function () {
     $('.search-btn').click(function () {
