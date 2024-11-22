66610f5b1793b7cabf3f1935311c818a34e9b769
Lallo Vigil <lallo.vigil@utsa.edu>
2024-11-22 16:55:12 -0600

    Added timeline updates

---
+++ src/js/timeline.js
@@ -0,0 +1,29 @@
+$(".form-check-input").on("click", function(e) {
+    var targetEle = e.currentTarget;
+    console.log(targetEle);
+  //   targetEle.checked=!targetEle.checked;
+    var catsChecked = 0;
+    $('.form-check-input').each(function (idx) {
+        if (this.checked) {
+            catsChecked = catsChecked + 1;
+        }
+    });
+    
+    var defaultBox = $('.default')
+    if (catsChecked > 0) {
+        $('.default').removeClass("show")
+    } else {
+        $('.default').addClass("show")
+    }
+    var scrollTargetClass = $("#" + targetEle.id).data("target");
+    setTimeout( function() {
+      scrollToSection(scrollTargetClass);
+    }, 400);
+  });
+  
+  function scrollToSection(scrollTargetClass) {
+    console.log("computed offset: " + $(scrollTargetClass).offset().top)
+    $([document.documentElement, document.body]).animate({
+          scrollTop: $(scrollTargetClass).offset().top
+      }, 800);
+  }
